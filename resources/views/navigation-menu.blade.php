78e008afa60dcb87343e32698593a307f0f773f0
Fais Salaeh <faiss.slh@gmail.com>
2023-11-09 14:59:41 +0700

    add Halaqah nav

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -15,7 +15,11 @@
                     <x-jet-nav-link href="{{ route('home') }}" :active="request()->routeIs('home')">
                         {{ __('Home') }}
                     </x-jet-nav-link>
-                    
+
+                    <x-jet-nav-link href="{{ route('halaqah') }}" :active="request()->routeIs('halaqah')">
+                        {{ __('Halaqah') }}
+                    </x-jet-nav-link>
+                                        
                     <x-jet-nav-link href="{{ route('feeds') }}" :active="request()->routeIs('feeds')">
                         {{ __('Feeds') }}
                     </x-jet-nav-link>
