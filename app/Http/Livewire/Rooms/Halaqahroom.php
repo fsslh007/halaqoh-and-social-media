848bbaae09425a9a09dee2980e151013c45e8472
Fais Salaeh <faiss.slh@gmail.com>
2023-11-11 23:33:37 +0700

    add Edit Halaqah button

--- app/Http/Livewire/Rooms/Halaqahroom.php
+++ app/Http/Livewire/Rooms/Halaqahroom.php
@@ -11,11 +11,24 @@ class Halaqahroom extends Component
     public $classroomName;
     public $room;
 
+    public $editRoomId;
+
     public function mount($roomId, $classroomName)
     {
         $this->roomId = $roomId;
         $this->classroomName = $classroomName;
         $this->room = Room::findOrFail($roomId);
+    }
+
+    public function editHalaqah($roomId)
+    {
+        // Your edit logic here...
+    
+        // Set the editRoomId variable
+        $this->editRoomId = $roomId;
+    
+        // Redirect to the "/rooms" page using a named route
+        return redirect()->route('rooms.index');
     }
 
     public function render()
--- resources/views/livewire/rooms/halaqahroom.blade.php
+++ resources/views/livewire/rooms/halaqahroom.blade.php
@@ -1,5 +1,7 @@
+<!-- resources/views/livewire/rooms/halaqahroom.blade.php -->
+
 <div class="flex items-center justify-center h-screen mt-8 mx-4">
-    <div class="bg-gray-200 p-8 rounded-lg w-full max-w-2xl">
+    <div class="p-8 rounded-lg w-full max-w-2xl">
         {{-- Big square covering all details --}}
         <div class="grid grid-cols-2 gap-4">
             {{-- Small square for Classroom Name --}}
@@ -12,9 +14,21 @@
                 <p class="text-lg"><strong>Meeting Time:</strong> {{ $room->meeting_time }}</p>
                 <p class="text-lg"><strong>Description:</strong> {{ $room->description }}</p>
                 <p class="text-lg"><strong>Leaving URL:</strong> {{ $room->leaving_url }}</p>
-            </div>
+                {{-- Add other fields as needed --}}
 
-            {{-- Add other fields as needed --}}
+                <!-- Edit Halaqah Button -->
+                <div class="mt-4 text-center relative">
+                    <button 
+                        wire:click="editHalaqah('{{ $editRoomId }}')"
+                        class="bg-black text-white px-4 py-2 rounded-md focus:outline-none relative z-10"
+                        style="position: relative; z-index: 10; background-color: black;"
+                    >
+                        {{ __('Edit Halaqah') }}
+                        <div class="absolute top-0 left-0 h-full w-full bg-black opacity-75 z-0"></div>
+                    </button>
+                </div>
+
+            </div>
         </div>
     </div>
 </div>
