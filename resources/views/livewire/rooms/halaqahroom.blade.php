9d9c26cc7502c3eda03e658f00538279d453674f
Fais Salaeh <faiss.slh@gmail.com>
2023-11-13 15:14:03 +0700

    Update halaqahroom.blade.php

--- resources/views/livewire/rooms/halaqahroom.blade.php
+++ resources/views/livewire/rooms/halaqahroom.blade.php
@@ -6,7 +6,7 @@
         <div class="grid grid-cols-2 gap-4">
             {{-- Small square for Classroom Name --}}
             <div class="p-4 bg-white shadow-md rounded-lg col-span-2 mx-2 text-center">
-                <h1 style="font-size: 1.5rem; font-weight: bold;" class="mb-2">Classroom: {{ $classroomName }}</h1>
+                <h1 style="font-size: 1.5rem; font-weight: bold;" class="mb-2">Halaqah: {{ $classroomName }}</h1>
             </div>
 
             {{-- Small square for Meeting Time, Description, and Leaving URL --}}
@@ -26,7 +26,7 @@
 
                 <p class="text-lg"><strong>Meeting Time:</strong> {{ $room->meeting_time }}</p>
                 <p class="text-lg"><strong>Description:</strong> {{ $room->description }}</p>
-                <p class="text-lg"><strong>Leaving URL:</strong> {{ $room->leaving_url }}</p>
+                <p class="text-lg"><strong>Meeting URL:</strong> {{ $room->leaving_url }}</p>
                 
                 {{-- Add other fields as needed --}}
                 
@@ -52,7 +52,7 @@
                         wire:click="showDeleteRoomModal({{ $room->id }})"
                         class="bg-red-600 text-white px-4 py-2 rounded-md focus:outline-none"
                     >
-                        {{ __('Delete Classroom') }}
+                        {{ __('Delete Halaqah') }}
                     </button>
                     @endcan
 
