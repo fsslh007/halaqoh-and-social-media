708f189aa863a4d2d6419b71a0fc55845054c4a4
Fais Salaeh <faiss.slh@gmail.com>
2023-11-13 15:27:43 +0700

    Update Uploadfile Model

--- app/Models/UploadFile.php
+++ app/Models/UploadFile.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class UploadFile extends Model
 {
     use HasFactory;
+
+        /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id',
+        'file_name',
+        'path',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
