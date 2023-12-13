181bc3752c574e6b503a17de9f44bb0b8089e58e
Fais Salaeh <faiss.slh@gmail.com>
2023-12-13 11:11:04 +0700

    Edit css for auth. Make the text in placeholder

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -16,13 +16,11 @@
             @csrf
 
             <div>
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required placeholder="{{ __('Email address') }}" required autofocus />
             </div>
 
             <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
+                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required placeholder="{{ __('Password') }}" required autocomplete="current-password" />
             </div>
 
             <div class="block mt-4">
