0f9a1649e1e25f82dddc043a02c65db8504f1570
Fais Salaeh <faiss.slh@gmail.com>
2023-12-14 18:04:24 +0700

    Create Meet model with migration files
    Create Meet model with migration files and add their field that needed

---
+++ database/migrations/2023_12_14_063817_create_meets_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('meets', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('room_id');
+            $table->string('meet_name');
+            $table->date('meet_date');
+            $table->time('meet_time');
+            $table->string('meet_location');
+            $table->timestamps();
+
+            // Define foreign key constraints
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('room_id')->references('id')->on('rooms')->onDelete('cascade');
+            // Assuming username exists in the 'users' table
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('meets');
+    }
+};
