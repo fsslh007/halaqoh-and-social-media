0346b90ee6cddd74d6161d30c21dde38ce6255b0
Fais Salaeh <faiss.slh@gmail.com>
2023-12-17 23:56:33 +0700

    Add toggle for showing or hiding detail for meets

--- app/Http/Livewire/Rooms/Halaqahroom.php
+++ app/Http/Livewire/Rooms/Halaqahroom.php
@@ -4,100 +4,48 @@ namespace App\Http\Livewire\Rooms;
 
 use Livewire\Component;
 use App\Models\Room;
-use App\Models\Meet; // Add the Meet model
-use Livewire\WithFileUploads;
+use App\Models\Meet;
 
 class Halaqahroom extends Component
 {
-    use WithFileUploads;
-
-    // Properties
     public $roomId;
-    public $classroomName;
-    public $room;
-    public $file;
-    public $uploadedFiles; // Variable name corrected
-
-    // Initialize the $room property with empty values
-    public function data()
-    {
-        return [
-            'room' => [
-                'name' => '',
-                'meeting_time' => '',
-                'description' => '',
-                'meeting_url' => '',
-                // Additional properties as needed
-            ],
-        ];
-    }
-
-    // Validation rules
-    protected $rules = [
-        'file' => 'nullable|file|max:10240',
-        // Additional validation rules as needed
-    ];
-
-    // Mount method to initialize component
+    public $meets = [];
+    public $showDetails = [];
+    
     public function mount($roomId, $classroomName)
     {
         $this->roomId = $roomId;
-        $this->classroomName = $classroomName;
-        $this->room = Room::findOrFail($roomId);
-        $this->getUploadedFiles(); // Initialize the uploadedFiles variable
-        $meets = $this->getMeets();
-    }
-
-    // Render method to display the Livewire component
-    public function render()
-    {
-        $meets = $this->getMeets(); // Fetch meets associated with the room
-        return view('livewire.rooms.halaqahroom', ['meets' => $meets]);
-    }
-
-    // Upload file
-    public function uploadFile()
-    {
-        // Validate the file upload
-        $this->validate([
-            'file' => 'nullable|file|max:10240', // Adjust the max file size as needed
-        ]);
-
-        // Check if a file is provided
-        if ($this->file) {
-            // Store the uploaded file
-            $file = $this->file->store('upload-files', 'public');
-
-            // Create a new UploadFile record
-            $this->room->uploadFiles()->create([ // Fixed method name
-                'user_id' => auth()->id(),
-                'file_name' => $this->file->getClientOriginalName(),
-                'path' => $file,
-            ]);
-
-            // Clear the file input
-            $this->file = null;
-
-            // Refresh the Livewire component
-            $this->emit('refreshLivewireComponent');
-
-            // Fetch and update the uploaded files
-            $this->getUploadedFiles();
-        }
-    }
-
-    // New method to fetch uploaded files
-    public function getUploadedFiles()
-    {
-        $this->uploadedFiles = $this->room->uploadFiles()->with('user')->get();    
-    }
-
-    public function getMeets()
-    {
-        // Fetch meets related to the current room and order them by date in descending order (newest to oldest)
-        return Meet::where('room_id', $this->roomId)
+        $this->meets = Meet::where('room_id', $this->roomId)
             ->orderBy('created_at', 'desc')
-            ->get();
+            ->get()
+            ->toArray();
+    
+        // Initialize showDetails array with all keys set to true
+        $this->showDetails = array_fill_keys(array_column($this->meets, 'id'), true);
     }
     
+    
+    public function fetchMeets()
+    {
+        $this->meets = Meet::where('room_id', $this->roomId)
+            ->orderBy('created_at', 'desc')
+            ->get()
+            ->toArray();
+    
+        // Initialize showDetails array with all keys set to true
+        $this->showDetails = array_fill_keys(array_column($this->meets, 'id'), true);
+    }
+    
+    
+    
+    public function toggleDetails($meetId)
+    {
+        $this->showDetails[$meetId] = !$this->showDetails[$meetId];
+    }
+          
+    
+    public function render()
+    {
+        return view('livewire.rooms.halaqahroom');
+    }
 }
--- resources/views/livewire/rooms/halaqahroom.blade.php
+++ resources/views/livewire/rooms/halaqahroom.blade.php
@@ -2,151 +2,54 @@
     <div class="bg-green-100 border my-3 border-green-400 text-green-700 dark:bg-green-700 dark:border-green-600 dark:text-green-100 px-4 py-3 rounded relative" role="alert">
         <span class="block sm:inline text-center">{{ session()->get('success') }}</span>
     </div>
-    @endif
+@endif
 
-    @if(session()->has('error'))
+@if(session()->has('error'))
     <div class="bg-red-100 border my-3 border-red-400 text-red-700 dark:bg-red-700 dark:border-red-600 dark:text-red-100 px-4 py-3 rounded relative" role="alert">
         <span class="block sm:inline text-center">{{ session()->get('error') }}</span>
     </div>
 @endif
 
-@foreach ($meets as $meet)
 <div class="flex flex-col mx-2 my-5 md:mx-6 md:my-12 lg:my-12 lg:w-2/5 lg:mx-auto">
-    <div class="bg-white shadow-md">
-        <div style="background-color: orange; display: flex; justify-content: left; align-items: center;">
-            <button wire:click="" class="mb-2" style="font-weight: bold; display: inline-block;">{{ $meet->meet_name }} || {{$meet->meet_date}}</button>
-        </div>
-            <div class="flex mb-4">
-                <div class="bg-green-100 w-full p-4 bg-white shadow-md rounded-lg mt-1 mx-2 text-center">
-                    <h3 style="font-weight: bold;">Date</h3>
-                    <div>{{ $meet->meet_date }}</div>
-                </div>
-                <div class="bg-green-100 w-full p-4 bg-white shadow-md rounded-lg mt-1 mx-2 text-center">
-                    <h3 style="font-weight: bold;">Time</h3>
-                    <div>{{ $meet->meet_time }}</div>
-                </div>
-                <div class="bg-green-100 w-full p-4 bg-white shadow-md rounded-lg mt-1 mx-2 text-center">
-                    <h3 style="font-weight: bold;">Place or URL</h3>
-                    <div>{{ $meet->meet_location }}</div>
-                </div>
-            </div>
-    </div>
-</div>
-@endforeach
-
-<!-- @if($meets->count() > 0) -->
-<!-- @else
-    <p>No meets available for this room.</p>
-@endif -->
-
-<!-- <div class="flex flex-col mx-2 my-5 md:mx-6 md:my-12 lg:my-12 lg:w-2/5 lg:mx-auto">
-    <div class="bg-white shadow-md rounded-3xl p-4">
-        <div class="flex-none"></div>
-            {{-- Big square covering all details --}}
-            <div class="grid grid-cols-1 sm:grid-cols-2 ">
-                {{-- Small square for Classroom Name --}}
-                <div class="p-4 bg-white shadow-md rounded-lg col-span-2 sm:col-span-1 mt-1 mx-2 text-center">
-                    <h1 style="font-size: 1.5rem; font-weight: bold;" class="mb-2">HALAQAH: {{ $classroomName }}</h1>
-                </div>
-
-                {{-- Small square for Meeting Time, Description, and Leaving URL --}}
-                <div class="p-4 bg-white shadow-md rounded-lg  mt-1 mx-2"> -->
-
-                    <!-- Show owner created Halaqah -->
-                    <!-- <div class="w-full flex-none mb-2 text-xs text-blue-700 font-medium" wire:offline.class.remove="text-blue-700" wire:offline.class="text-gray-400">
-                        @if ($room->user)
-                            <a href="{{ route('profile', ['username' => $room->user->username]) }}">
-                                <img class="inline-block object-cover w-8 h-8 mr-1 text-white rounded-full shadow-sm cursor-pointer" wire:offline.class="filter grayscale" src="{{ $room->user->profile_photo_url }}" alt="{{ $room->user->name }}" />
-                                Created by {{ '@' . $room->user->username }}
-                            </a>
+    @if (!empty($meets))
+        @foreach ($meets as $meet)    
+            <div class="bg-white shadow-md" style="margin-bottom: 30px;">
+                <button wire:click="toggleDetails({{ $meet['id'] }})" class="py-1"style="background-color: orange; display: flex; justify-content: left; align-items: center;  width: 100%;">
+                    <div class="" style="font-weight: bold; display: inline-block; padding: 5px 10px; text-align: center;">
+                        {{ $meet['meet_name'] }} || {{ $meet['meet_date'] }}
+                    </div>
+                </button>
+                @if (isset($showDetails[$meet['id']]) && $showDetails[$meet['id']])
+                    <div wire:key="details-{{ $meet['id'] }}" class="details-container">
+                        @if (isset($meet['meet_date']))
+                            <div class="flex mb-4"  style="margin-top: 10px;">
+                                <div class="flex-1 bg-green-100 p-4 bg-white shadow-md rounded-lg mt-1 mx-2 text-center">
+                                    <h3 style="font-weight: bold;">Date</h3>
+                                    <div>{{ $meet['meet_date'] }}</div>
+                                </div>
+                                @if (isset($meet['meet_time']))
+                                    <div class="flex-1 bg-green-100 p-4 bg-white shadow-md rounded-lg mt-1 mx-2 text-center">
+                                        <h3 style="font-weight: bold;">Time</h3>
+                                        <div>{{ $meet['meet_time'] }}</div>
+                                    </div>
+                                @endif
+                                @if (isset($meet['meet_location']))
+                                    <div class="flex-1 bg-green-100 p-4 bg-white shadow-md rounded-lg mt-1 mx-2 text-center">
+                                        <h3 style="font-weight: bold;">Place or URL</h3>
+                                        <div>{{ $meet['meet_location'] }}</div>
+                                    </div>
+                                @endif
+                            </div>
                         @else
-                            <span class="text-gray-500">Creator information not available</span>
+                            <p>No details available for this meet.</p>
                         @endif
-                    </div>
-                    
-                    <p class="text-lg"><strong>Description:</strong> {{ $room->description }}</p>
-                    <p class="text-lg"><strong>Meeting Time:</strong> {{ $room->meeting_time }}</p>
-                    <p class="text-lg"><strong>Meeting URL:</strong> {{ $room->leaving_url }}</p>
-                    
-                    {{-- Add other fields as needed --}}
-                    
-            </div>
-        </div>
-    </div>
-</div> -->
-
-
-<!-- <div class="flex flex-col mx-2 my-5 md:mx-6 md:my-12 lg:my-12 lg:w-2/5 lg:mx-auto">
-    <div class="bg-white shadow-md rounded-3xl p-4">
-        <div class="flex-none">
-            <div class="p-4 bg-white shadow-md rounded-lg mx-2 mt-1">
-                <h2 style="font-size: 1.5rem; font-weight: bold;" class="mb-2">File Upload</h2> -->
-
-                <!-- Livewire File Upload Form -->
-                <!-- <form wire:submit.prevent="uploadFile" enctype="multipart/form-data">
-                    <div class="mb-4">
-                        <x-jet-label for="file" value="{{ __('Choose File') }}" />
-                        <x-jet-input id="file" type="file" class="mt-1 block w-full" wire:model="file" />
-                        <x-jet-input-error for="file" class="mt-2" />
-                    </div>
-                    <div class="flex items-center justify-end mt-4">
-                        <x-jet-button type="submit" wire:loading.attr="disabled">
-                            {{ __('Upload File') }}
-                        </x-jet-button>
-                    </div>
-                </form>
-            </div>    
-            <div class="p-4 bg-white shadow-md rounded-lg mx-2 mt-1 overflow-x-auto"> -->
-
-                <!-- Add this section to display uploaded files in a table -->
-                <!-- @if ($uploadedFiles && $uploadedFiles->count() > 0)
-                    <div class="mt-4">
-                        <h2 class="text-lg font-semibold mb-2">Uploaded Files:</h2>
-                        <table class="min-w-full divide-y divide-gray-200">
-                            <thead class="bg-gray-50">
-                                <tr>
-                                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                        File Name
-                                    </th>
-                                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                        Uploaded By
-                                    </th>
-                                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                        Uploaded Date
-                                    </th>
-                                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                        Actions
-                                    </th>
-                                </tr>
-                            </thead>
-                            <tbody class="bg-white divide-y divide-gray-200">
-                                @foreach ($uploadedFiles as $file)
-                                    <tr>
-                                        <td class="px-6 py-4 whitespace-nowrap">
-                                            <a href="{{ asset('storage/' . $file->path) }}" target="_blank" class="text-red-500 hover:underline">
-                                                {{ $file->file_name }}
-                                            </a>
-                                        </td>
-                                        <td class="px-6 py-4 whitespace-nowrap">
-                                            {{ $file->user->name ?? 'N/A' }}
-                                        </td>
-                                        <td class="px-6 py-4 whitespace-nowrap">
-                                            {{ $file->created_at->format('M d, Y H:i:s') }}
-                                        </td>
-                                        <td class="px-6 py-4 whitespace-nowrap"> -->
-
-                                            <!-- Add a delete button -->
-                                            <!-- <button wire:click="deleteFile({{ $file->id }})" class="text-red-500hover:underline">
-                                                Delete
-                                            </button>
-                                        </td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
                     </div>
                 @endif
             </div>
+        @endforeach
+    @else
+        <div class="bg-red-100 border my-3 border-red-400 text-red-700 dark:bg-red-700 dark:border-red-600 dark:text-red-100 px-4 py-3 rounded relative" role="alert">
+            <span class="block sm:inline text-center">{{ 'No meets found.' }}</span>
         </div>
-    </div>
-</div> -->
+    @endif
+</div>
