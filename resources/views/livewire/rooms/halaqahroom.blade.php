e5003cf316dc6758a9fc3bb8c34d92d6351446dd
Fais Salaeh <faiss.slh@gmail.com>
2023-12-23 21:24:15 +0700

    Update Halqahroom css

--- resources/views/livewire/rooms/halaqahroom.blade.php
+++ resources/views/livewire/rooms/halaqahroom.blade.php
@@ -1,21 +1,25 @@
 @if(session()->has('success'))
-    <div class="bg-green-100 border my-3 border-green-400 text-green-700 dark:bg-green-700 dark:border-green-600 dark:text-green-100 px-4 py-3 rounded relative" role="alert">
-        <span class="block sm:inline text-center">{{ session()->get('success') }}</span>
+    <div class="relative px-4 py-3 my-3 text-green-700 bg-green-100 border border-green-400 rounded dark:bg-green-700 dark:border-green-600 dark:text-green-100" role="alert">
+        <span class="block text-center sm:inline">{{ session()->get('success') }}</span>
     </div>
 @endif
 
 @if(session()->has('error'))
-    <div class="bg-red-100 border my-3 border-red-400 text-red-700 dark:bg-red-700 dark:border-red-600 dark:text-red-100 px-4 py-3 rounded relative" role="alert">
-        <span class="block sm:inline text-center">{{ session()->get('error') }}</span>
+    <div class="relative px-4 py-3 my-3 text-red-700 bg-red-100 border border-red-400 rounded dark:bg-red-700 dark:border-red-600 dark:text-red-100" role="alert">
+        <span class="block text-center sm:inline">{{ session()->get('error') }}</span>
     </div>
 @endif
 
 <div class="flex flex-col mx-2 my-5 md:mx-6 md:my-12 lg:my-12 lg:w-2/5 lg:mx-auto">
     @if (!empty($meets))
         @foreach ($meets as $meet)    
-            <div class="bg-white shadow-md" style="margin-bottom: 30px;">
-                <button wire:click="toggleDetails({{ $meet['id'] }})" class="py-1"style="background-color: orange; display: flex; justify-content: left; align-items: center;  width: 100%;">
-                    <div class="" style="font-weight: bold; display: inline-block; padding: 5px 10px; text-align: center;">
+            <div class="bg-white shadow-md mb-7">
+                <button wire:click="toggleDetails({{ $meet['id'] }})" class="flex items-center w-full py-1 bg-orange-300">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" 
+                        class="w-5 h-5">
+                        <path fill-rule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clip-rule="evenodd" />
+                    </svg>
+                    <div style="font-weight: bold; display: inline-block; padding: 5px 10px; text-align: center;">
                         {{ $meet['meet_name'] }} || {{ $meet['meet_date'] }}
                     </div>
                 </button>
@@ -23,18 +27,18 @@
                     <div wire:key="details-{{ $meet['id'] }}" class="details-container">
                         @if (isset($meet['meet_date']))
                             <div class="flex mb-4"  style="margin-top: 10px;">
-                                <div class="flex-1 bg-green-100 p-4 bg-white shadow-md rounded-lg mt-1 mx-2 text-center">
+                                <div class="flex-1 p-4 mx-2 mt-1 text-center bg-white rounded-lg shadow-md">
                                     <h3 style="font-weight: bold;">Date</h3>
                                     <div>{{ $meet['meet_date'] }}</div>
                                 </div>
                                 @if (isset($meet['meet_time']))
-                                    <div class="flex-1 bg-green-100 p-4 bg-white shadow-md rounded-lg mt-1 mx-2 text-center">
+                                    <div class="flex-1 p-4 mx-2 mt-1 text-center bg-white rounded-lg shadow-md">
                                         <h3 style="font-weight: bold;">Time</h3>
                                         <div>{{ $meet['meet_time'] }}</div>
                                     </div>
                                 @endif
                                 @if (isset($meet['meet_location']))
-                                    <div class="flex-1 bg-green-100 p-4 bg-white shadow-md rounded-lg mt-1 mx-2 text-center">
+                                    <div class="flex-1 p-4 mx-2 mt-1 text-center bg-white rounded-lg shadow-md">
                                         <h3 style="font-weight: bold;">Place or URL</h3>
                                         <div>{{ $meet['meet_location'] }}</div>
                                     </div>
@@ -48,8 +52,8 @@
             </div>
         @endforeach
     @else
-        <div class="bg-red-100 border my-3 border-red-400 text-red-700 dark:bg-red-700 dark:border-red-600 dark:text-red-100 px-4 py-3 rounded relative" role="alert">
-            <span class="block sm:inline text-center">{{ 'No meets found.' }}</span>
+        <div class="relative px-4 py-3 my-3 text-red-700 bg-red-100 border border-red-400 rounded dark:bg-red-700 dark:border-red-600 dark:text-red-100" role="alert">
+            <span class="block text-center sm:inline">{{ 'No meets found.' }}</span>
         </div>
     @endif
 </div>
--- resources/views/rooms/halaqahroom.blade.php
+++ resources/views/rooms/halaqahroom.blade.php
@@ -1,26 +1,57 @@
 <x-app-layout>
     <x-slot name="header">
         <div class="flex items-center justify-between">
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                {{ __('Halaqah Room - ' . $classroomName) }}
-            </h2>
-            <div class="flex items-center space-x-4">
+            <a class="flex items-center space-x-2">
+                <button class="px-1 py-2 bg-orange-200 rounded-sm text-sm text-gray-600 hover:text-gray-900 focus:outline-none flex items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
+                        <path fill-rule="evenodd" d="M12.79 5.23a.75.75 0 01-.02 1.06L8.832 10l3.938 3.71a.75.75 0 11-1.04 1.08l-4.5-4.25a.75.75 0 010-1.08l4.5-4.25a.75.75 0 011.06.02z" clip-rule="evenodd" />
+                    </svg>
+                </button>
+                <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                    {{ __($classroomName) }}
+                </h2>
+                <button>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
+                        <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a.75.75 0 000 1.5h.253a.25.25 0 01.244.304l-.459 2.066A1.75 1.75 0 0010.747 15H11a.75.75 0 000-1.5h-.253a.25.25 0 01-.244-.304l.459-2.066A1.75 1.75 0 009.253 9H9z" clip-rule="evenodd" />
+                    </svg>
+                </button>
+            </a>
+            <div class="flex items-center">
                 @can('update', $room)
-                <span>
+                <span class="flex items-center">
+                    <!-- Create Meet -->
                     <a href="{{ route('rooms.createMeet', ['room' => $id, 'classroomName' => $classroomName]) }}" 
                         :active="request()->routeIs('rooms.createMeet', ['room' => $id, 'classroomName' => $classroomName])'"
-                        class="text-sm text-gray-600 hover:text-gray-900 focus:outline-none ml-4">
-                            + Create Meet
+                        class="px-2 py-2 bg-green-400 rounded-lg text-sm text-gray-600 hover:text-gray-900 focus:outline-none flex items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
+                            <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm.75-11.25a.75.75 0 00-1.5 0v2.5h-2.5a.75.75 0 000 1.5h2.5v2.5a.75.75 0 001.5 0v-2.5h2.5a.75.75 0 000-1.5h-2.5v-2.5z" clip-rule="evenodd" />
+                        </svg>
+                        <span>{{__('Meet')}}</span>   
                     </a>
+                    
+                    <!-- Add some space -->
+                    <div class="w-2"></div>
+
+                    <!-- member --> 
                     <a href="{{ route('rooms.member', ['room' => $id, 'classroomName' => $classroomName]) }}" 
                         :active="request()->routeIs('rooms.member', ['room' => $id, 'classroomName' => $classroomName])'"
-                        class="text-sm text-gray-600 hover:text-gray-900 focus:outline-none ml-4">
-                            Member
+                        class="px-2 py-2 bg-yellow-400 rounded-lg text-sm text-gray-600 hover:text-gray-900 focus:outline-none flex items-center ">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
+                            <path d="M11 5a3 3 0 11-6 0 3 3 0 016 0zM2.615 16.428a1.224 1.224 0 01-.569-1.175 6.002 6.002 0 0111.908 0c.058.467-.172.92-.57 1.174A9.953 9.953 0 018 18a9.953 9.953 0 01-5.385-1.572zM16.25 5.75a.75.75 0 00-1.5 0v2h-2a.75.75 0 000 1.5h2v2a.75.75 0 001.5 0v-2h2a.75.75 0 000-1.5h-2v-2z" />
+                        </svg>
+                        <span>{{__('Member')}}</span>
                     </a>
+
+                    <!-- Add some space -->
+                    <div class="w-6"></div>
+
+                    <!-- setting -->
                     <a href="{{ route('rooms.edit', ['room' => $id]) }}" 
-                        :active="request()->routeIs('rooms.edit', ['room' => $id])')" 
-                        class="text-sm text-gray-600 hover:text-gray-900 focus:outline-none ml-4">
-                            Settings
+                        :active="request()->routeIs('rooms.edit', ['room' => $id])'" 
+                        class="px-2 py-2 bg-gray-400 rounded-lg text-sm text-gray-600 hover:text-gray-900 focus:outline-none flex items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
+                            <path fill-rule="evenodd" d="M8.34 1.804A1 1 0 019.32 1h1.36a1 1 0 01.98.804l.295 1.473c.497.144.971.342 1.416.587l1.25-.834a1 1 0 011.262.125l.962.962a1 1 0 01.125 1.262l-.834 1.25c.245.445.443.919.587 1.416l1.473.294a1 1 0 01.804.98v1.361a1 1 0 01-.804.98l-1.473.295a6.95 6.95 0 01-.587 1.416l.834 1.25a1 1 0 01-.125 1.262l-.962.962a1 1 0 01-1.262.125l-1.25-.834a6.953 6.953 0 01-1.416.587l-.294 1.473a1 1 0 01-.98.804H9.32a1 1 0 01-.98-.804l-.295-1.473a6.957 6.957 0 01-1.416-.587l-1.25.834a1 1 0 01-1.262-.125l-.962-.962a1 1 0 01-.125-1.262l.834-1.25a6.957 6.957 0 01-.587-1.416l-1.473-.294A1 1 0 011 10.68V9.32a1 1 0 01.804-.98l1.473-.295c.144-.497.342-.971.587-1.416l-.834-1.25a1 1 0 01.125-1.262l.962-.962A1 1 0 015.38 3.03l1.25.834a6.957 6.957 0 011.416-.587l.294-1.473zM13 10a3 3 0 11-6 0 3 3 0 016 0z" clip-rule="evenodd" />
+                        </svg>
                     </a>
                 </span>
                 @endcan
