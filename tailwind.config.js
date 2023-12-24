361daddb77f54468d96ef54fe074443e8de2990f
Fais Salaeh <faiss.slh@gmail.com>
2023-12-24 10:29:30 +0700

    Install daisyUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -50,5 +50,9 @@ module.exports = {
         },
     },
 
-    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/typography'),
+        require("daisyui")
+    ],
 };
